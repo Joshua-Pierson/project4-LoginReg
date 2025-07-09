05757e4922b1529fb13a59e50638420c346fb059
Joshua Pierson <pierson.joshua726@gmail.com>
2025-07-08 23:48:53 -0500

    update

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ import loginroute from './Routers/login.js';
 import registerroute from './Routers/register.js';
 import categoryroute from './Routers/categories.js';
 import questionroute from './Routers/questions.js';
+import path from 'path';
 // Add and import your route files here, Example:
 // import yourRoute from './Routers/yourRoute.js'
 
 const app = express();
+app.use(express.static(path.join(process.cwd(), 'client/build')));
 
 app.use(express.json());  // To parse JSON bodies in requests
 app.use(cors(({
@@ -24,10 +26,7 @@ app.use('/questions', questionroute);
 // Add your route handlers here, Example:
 // app.use('/your-endpoint', yourRoute)
 
-app.get('/', (req, res) => {
-  res.send("Server running");
-});
+app.use(express.static(path.join(process.cwd(), 'client/build')));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(4000, () => {
-  console.log("Listening at http://localhost:4000");
-});
